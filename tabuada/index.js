fdd2e1557f78f44a5e5bb0e9dd24e032d34f1d4c
NelsonduarteGit <nelson.a.duarte@hotmail.com>
2021-09-01 11:26:43 +0100

    Tabuada

--- tabuada/index.js
+++ tabuada/index.js
@@ -1,14 +1,14 @@
 function tabuada() {
-    // vou buscar o valor do input e converto em int
-    let numero = parseInt(document.getElementById("numero").value);
-    // vou buscar a div para apresentar a tabuada
-    let resposta = document.getElementById("mostrar_tabuada");
-    // inicializo a variavel tabuada para guardar cada linha
-    let tabuada = "";
+  // vou buscar o valor do input e converto em int
+  let numero = parseInt(document.getElementById("numero").value);
+  // vou buscar a div para apresentar a tabuada
+  let resposta = document.getElementById("mostrar_tabuada");
+  // inicializo a variavel tabuada para guardar cada linha
+  let tabuada = "";
 
-    for (let i = 1; i <= 10; i++)
-        tabuada += numero + " x " + i + " = " + numero * i + "<br>";
+  for (let i = 1; i <= 10; i++)
+    tabuada += `${numero} x ${i} = ${numero * i} <br>`;
 
-    // insiro a tabuada no browser com o
-    resposta.innerHTML = tabuada;
+  // insiro a tabuada no browser com o
+  resposta.innerHTML = tabuada;
 }
